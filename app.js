efbdd6dd52e4de85d6ab123751c284b0f6938be5
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-15 11:50:19 -0700

    app.js
    Updated file

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const sum = require("./tutorial");
+const tutorial = require("./tutorial");
 
-console.log("Hello World from Node.js");
-sum(1,1);
+console.log(tutorial);
+// sum(1, 1); the function was imported as "tutorial" instead of sum, so this wouldn't work
+
+console.log(tutorial(1, 1));
+
+// console.log("Hello World from Node.js")
