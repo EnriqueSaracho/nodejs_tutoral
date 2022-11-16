dfebe9ff24b6c3d8098e0843866ee28096a64348
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-16 11:32:52 -0700

    app.js tutorial.js
    Updated files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const tutorial = require("./tutorial");
-
-console.log(tutorial);
-// sum(1, 1); the function was imported as "tutorial" instead of sum, so this wouldn't work
-
-console.log(tutorial(1, 1));
-
 // console.log("Hello World from Node.js")
+
+const tutorial = require("./tutorial");
+// console.log(tutorial);
+
+// console.log(sum(1, 1)); the function was imported as "tutorial" instead of sum, so this wouldn't work
+// console.log(tutorial(1, 1));
+
+// After multiple module.exports
+console.log(tutorial.sum(2, 3));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
