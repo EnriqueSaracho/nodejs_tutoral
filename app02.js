b88ab0fb9f9d2e65f97309e4af43a2283dda7ac2
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-16 11:52:00 -0700

    app02.js
    Created file, Subject: Event Emitter Class

---
+++ app02.js
@@ -0,0 +1,30 @@
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("tutorial", (num1, num2) => {
+  console.log(num1 * num2);
+});
+
+// eventEmitter.emit("tutorial", 2, 3);
+
+class Person extends EventEmitter {
+  constructor(name) {
+    super();
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+let kiki = new Person("Kiki");
+let katara = new Person("Katara");
+katara.on("name", () => {
+  console.log("My name is " + katara.name);
+});
+kiki.on("name", () => {
+  console.log("My name is " + kiki.name);
+});
+
+kiki.emit("name");
+katara.emit("name");
