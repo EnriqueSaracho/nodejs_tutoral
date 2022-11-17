da77d7e3e299c8429586a9d351247ac83832b6f6
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-17 12:34:34 -0700

    app03.js
    Updated file

--- app03.js
+++ app03.js
@@ -10,6 +10,16 @@ let answer = num1 + num2;
 rl.question(`What is ${num1} + ${num2}? \n`, (userInput) => {
   if (userInput.trim() == answer) {
     rl.close();
+  } else {
+    rl.setPrompt("Incorrect response, please try again \n");
+    rl.prompt();
+    rl.on("line", (userInput) => {
+      if (userInput.trim() == answer) rl.close();
+      else {
+        rl.setPrompt(`Your answer of ${userInput} is incorrect, try again \n`);
+        rl.prompt();
+      }
+    });
   }
 });
 
