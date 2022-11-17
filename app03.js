e583c70187c3663d18b17a1948fc5e5eb8226ef2
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-17 12:06:04 -0700

    app03.js
    Updated file, added Readline Module to get input from user

--- app03.js
+++ app03.js
@@ -1,2 +1,17 @@
 const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let num1 = Math.floor(Math.random() * 10 + 1);
+let num2 = Math.floor(Math.random() * 10 + 1);
+let answer = num1 + num2;
 
+rl.question(`What is ${num1} + ${num2}? \n`, (userInput) => {
+  if (userInput.trim() == answer) {
+    console.log("Correct!");
+  } else {
+    console.log("Wrong");
+  }
+  rl.close();
+});
