d575dab24bb9650932a68017c284a1c7e1a8a128
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-17 12:59:08 -0700

    file_system.js
    Updated file

--- file_system.js
+++ file_system.js
@@ -1,14 +1,31 @@
 const fs = require("fs");
 
-// create a file
-fs.writeFile("example.txt", "This is an example", (err) => {
+// // create a file
+// fs.writeFile("example.txt", "This is an example", (err) => {
+//   if (err) console.log(err);
+//   else {
+//     console.log("File successfully created");
+//     // read de content of the file
+//     fs.readFile("example.txt", "utf8", (err, file) => {
+//       if (err) console.log(err);
+//       else console.log(file);
+//     });
+//   }
+// });
+
+// // rename a file
+// fs.rename("example.txt", "example2.txt", (err) => {
+//   if (err) console.log(err);
+//   else console.log("Successfully renamed the file");
+// });
+
+// // append data to file
+// fs.appendFile("example2.txt", "Some data being appended", (err) => {
+//   if (err) console.log(err);
+//   else console.log("Successfully appended data to file");
+// });
+
+fs.unlink("example2.txt", (err) => {
   if (err) console.log(err);
-  else {
-    console.log("File successfully created");
-    // read de content of the file
-    fs.readFile("example.txt", "utf8", (err, file) => {
-      if (err) console.log(err);
-      else console.log(file);
-    });
-  }
+  else console.log("Successfully deleted file");
 });
