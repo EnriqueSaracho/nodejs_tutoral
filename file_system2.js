099af4808ed98b9baf09d52038fed8b041afbf57
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-18 10:22:12 -0700

    file_system2.js
    Updated file, deleted tutorial folder with node.js

--- file_system2.js
+++ file_system2.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
-fs.mkdir("tutorial", (err) => {
+
+// // create folder
+// fs.mkdir("tutorial", (err) => {
+//   if (err) console.log(err);
+//   else console.log("Folder successfully created");
+// });
+
+// delete folder
+fs.rmdir("tutorial", (err) => {
   if (err) console.log(err);
-  else console.log("folder successfully created");
+  else console.log("Folder deleted succesfully");
 });
