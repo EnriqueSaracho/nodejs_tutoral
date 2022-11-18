763320be6c9c0eedf4ada3cbf21f96e939e4e956
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-18 12:57:38 -0700

    pipe_chaining.js
    Updated file, created uncompressed file with node.js

--- pipe_chaining.js
+++ pipe_chaining.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
-const readStream = fs.createReadStream("./example.txt", "utf8");
-const writeStream = fs.createWriteStream("./example2.txt");
-readStream.pipe(writeStream);
+const zlib = require("zlib");
+// const gzip = zlib.createGzip();
+const gunzip = zlib.createGzip();
+// const readStream = fs.createReadStream("./example.txt", "utf8");
+// const writeStream = fs.createWriteStream("./example2.txt.gz");
+// readStream.pipe(gzip).pipe(writeStream);
+const readStream = fs.createReadStream("./example2.txt.gz");
+const writeStream = fs.createWriteStream("uncompressed.txt");
+readStream.pipe(gunzip).pipe(writeStream);
