8b8b6fe1b384b3e73e40bee98857ac876b971cf1
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-18 10:35:18 -0700

    file_system2.js
    Updated file, deleted previously created folder

--- file_system2.js
+++ file_system2.js
@@ -12,13 +12,24 @@ const fs = require("fs");
 //   else console.log("Folder deleted succesfully");
 // });
 
-// create folder and file inside
-fs.mkdir("tutorial", (err) => {
+// // create folder and file inside
+// fs.mkdir("tutorial", (err) => {
+//   if (err) console.log(err);
+//   else {
+//     fs.writeFile("./tutorial/example.txt", "123", (err) => {
+//       if (err) console.log(err);
+//       else console.log("Succesfully created folder and file");
+//     });
+//   }
+// });
+
+// delete file and then folder (has to be in that order)
+fs.unlink("./tutorial/example.txt", (err) => {
   if (err) console.log(err);
   else {
-    fs.writeFile("./tutorial/example.txt", "123", (err) => {
+    fs.rmdir("tutorial", (err) => {
       if (err) console.log(err);
-      else console.log("Succesfully created folder and file");
+      else console.log("File and folder deleted successfully");
     });
   }
 });
