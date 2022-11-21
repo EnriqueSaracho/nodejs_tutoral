421847abae9d4b6902512aa55d125d5cb77da0be
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-21 12:55:04 -0700

    events.js
    Updated file

--- events.js
+++ events.js
@@ -5,7 +5,7 @@ eventEmitter.on("tutorial", (num1, num2) => {
   console.log(num1 * num2);
 });
 
-// eventEmitter.emit("tutorial", 2, 3);
+eventEmitter.emit("tutorial", 2, 3);
 
 class Person extends EventEmitter {
   constructor(name) {
@@ -28,3 +28,4 @@ kiki.on("name", () => {
 
 kiki.emit("name");
 katara.emit("name");
+kiki.emit("name");
